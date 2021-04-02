ef4224dd2abebd0f5ebaf9f807626f0ec838fa09
chris-revell <christopher.revell@manchester.ac.uk>
2021-04-02 19:58:34 +0100

    Moving parameters to argument

--- Simulate.jl
+++ Simulate.jl
@@ -19,20 +19,20 @@ using NumericalIntegration
 using Model
 using CreateRunDirectory
 
-@inline @views function simulate()
+@inline @views function simulate(N,α,γ,tMax)
 
-    N      = 50         # Number of grid pointsin each dimension
+    #N      = 100         # Number of grid pointsin each dimension
     h      = 1/(N-1)    # Spatial separation of grid points
-    α      = 0.5        # Diffusion coefficient
-    γ      = 0.0005    # sqrt(γ) gives the length of the transition regions between domains
-    tMax   = 10.0
+    #α      = 0.5        # Diffusion coefficient
+    #γ      = 0.0005    # sqrt(γ) gives the length of the transition regions between domains
+    #tMax   = 5.0
     outInt = tMax/100.0 # Output interval
     tspan  = (0.0,tMax) # Time span for solution
 
     # Create output files
     folderName = createRunDirectory(N,h,α,γ,outInt,tMax)
 
-    # Random initial distribution    
+    # Random initial distribution
     u0    = 2.0.*rand(N+4,N+4).-1.0
     ∇²u   = zeros(N+4,N+4)
     part1 = zeros(N+4,N+4)
