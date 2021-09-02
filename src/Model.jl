d63dd2edf212fa5b0ca6bf3d7b9bc8a698e5b375
chris-revell <christopher.revell@manchester.ac.uk>
2021-09-02 14:01:19 +0100

    Starting split model

--- src/Model.jl
+++ src/Model.jl
@@ -22,7 +22,37 @@ include("Div.jl"); using .Div
 # ϕ̃ₜ = α∇²(rϕ + ∇²(∇²ϕ + q²ϕ) + q⁴ + ϕ³)
 # From Thiele, Knobloch 2013 Equation 3
 
-@views function PFC!(du, u, p, t)
+function f1!(du, u, p, t)
+
+    # Unpack parameter list
+    deriv, part1, part2, N, h, αᵢ, αⱼ, r, a, q, q², q⁴, graduᵢ, graduⱼ = p
+
+    # Set values of ghost points to ensure zero flux at boundary
+    boundaryConditions!(u,N,h)
+
+    # Find Laplacian of u
+    ∇²!(deriv,u,N,h,0)
+
+    # Calculate inner component (∇²ϕ + q²ϕ)
+    @tturbo part1 .= deriv .+ q².*u
+
+    # Find Laplacian of (∇²ϕ + q²ϕ)
+    ∇²!(deriv, part1, N, h, 1)
+
+    # Calculate full term within outermost Laplacian (rϕ + ∇²(∇²ϕ + q²ϕ) + q⁴ + ϕ³) = rϕ + ∇²(part1) + q⁴ + ϕ³
+    @tturbo part2 .= r.*u .+ deriv .+ q⁴ .+ u.^3
+
+    # Find grad of (rϕ + ∇²(∇²ϕ + q²ϕ) + q⁴ + ϕ³) and multiply by spatially varying diffusivity
+    grad!(graduᵢ, graduⱼ, part2, αᵢ, αⱼ, N, h)
+
+    # Find divergence of the result from the last term
+    div!(du, graduᵢ, graduⱼ, N, h)
+
+    return du
+
+end
+
+function f2!(du, u, p, t)
 
     # Unpack parameter list
     deriv, part1, part2, N, h, αᵢ, αⱼ, r, q, q², q⁴, graduᵢ, graduⱼ = p
@@ -52,6 +82,6 @@ include("Div.jl"); using .Div
 
 end
 
-export PFC!
+export f1!, f2!
 
 end
