98dae8da0c1d85afc07ee9a3aa4417d5baa475e4
Christopher Revell <christopher.revell@manchester.ac.uk>
2021-12-01 10:56:47 +0000

    Update notebookImagesBinarizeSegment.jl

--- notebooks/notebookImagesBinarizeSegment.jl
+++ notebooks/notebookImagesBinarizeSegment.jl
@@ -6,6 +6,7 @@ using ImageTransformations
 using ImageView
 using ImageSegmentation
 using Random
+using Base.Filesystem
 
 function get_random_color(seed)
     Random.seed!(seed)
@@ -20,77 +21,49 @@ function maskColour(i,seg)
     end
 end
 
-#imagePath = "$(homedir())/Dropbox (The University of Manchester)/EM-images/mmp13ko3view/mmp13ko-3wiew_4800X_hui_0002.tif";
-imagePath = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_raw/mmp13ko-3wiew_4800X_hui_0002.png";
 
-image = load(imagePath)
+fileDirectory = "/Users/christopher/Dropbox (The University of Manchester)/EM-images/mmp13ko3view"
+
 
 binarizeFuns = [Intermodes(),
-    Moments(),
     Otsu(),
     Polysegment(),
-    UnimodalRosin(),
-    #Sauvola(),
-    #MinimumError(),
-    #Niblack(),
-    #Yen(),
-    #Balanced(),
-    #AdaptiveThreshold(image;window_size=50,percentage=10)
 ]
 
 binarizeFunNames = ["Intermodes",
-    "Moments",
     "Otsu",
     "Polysegment",
-    "UnimodalRosin",
-    #"Sauvola",
-    #"MinimumError",
-    #"Niblack()",
-    #"Yen()",
-    #"Balanced()",
-    #"AdaptiveThreshold(image;window_size=50,percentage=10"
-]
-
-segmentFuns = [
-    #"unseeded_region_growing",
-    "felzenszwalb",
-    #"meanshift",
-    "fast_scanning"
 ]
 
 
-distance = 10.0
-filteredImage  = Gray.(image) #imfilter(Gray.(image),Kernel.gaussian(distance))
-binarizedImage = copy(filteredImage)
-segmentedImage = copy(image)
+distance = 2.0
 
-for (n,fun) in enumerate(binarizeFuns)
+files = [f for f in readdir(fileDirectory) if f[end-2:end]=="png"]
 
-    binarizedImage .= binarize(filteredImage,fun)
-    saveName = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/binarizeFuns/$(binarizeFunNames[n]).png"
-    save(saveName,binarizedImage)
+for f in files
+    mkpath("$fileDirectory/$(f[1:end-4])")
 
-    seg = felzenszwalb(binarizedImage, 300, 100)
-    segmentedImage .= map(i->get_random_color(i), labels_map(seg))
-    saveName = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/binarizeFuns/$(binarizeFunNames[n])Felzenszwalb.png"
-    save(saveName,segmentedImage)
-    seg2 = prune_segments(seg, i->(segment_pixel_count(seg,i)<30000), (i,j)->(-segment_pixel_count(seg,j)))
-    segmentedImage .= map(i->get_random_color(i), labels_map(seg2))
-    saveName = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/binarizeFuns/$(binarizeFunNames[n])FelzenszwalbPruned.png"
-    save(saveName,segmentedImage)
-    segmentedImage .= map(i->maskColour(i,seg2), labels_map(seg2))
-    saveName = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/binarizeFuns/$(binarizeFunNames[n])FelzenszwalbPrunedMask.png"
-    save(saveName,segmentedImage)
+    image = load("$fileDirectory/$f")
+    filteredImage  = imfilter(Gray.(image),Kernel.gaussian(distance))
+    binarizedImage = copy(filteredImage)
+    segmentedImage = copy(image)
 
-    seg = fast_scanning(binarizedImage, 0.1)
-    segmentedImage .= map(i->get_random_color(i), labels_map(seg))
-    saveName = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/binarizeFuns/$(binarizeFunNames[n])FastScanning.png"  # threshold = 0.1
-    save(saveName,segmentedImage)
-    seg2 = prune_segments(seg, i->(segment_pixel_count(seg,i)<30000), (i,j)->(-segment_pixel_count(seg,j)))
-    segmentedImage .= map(i->get_random_color(i), labels_map(seg2))
-    saveName = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/binarizeFuns/$(binarizeFunNames[n])FastScanningPruned.png"
-    save(saveName,segmentedImage)
-    segmentedImage .= map(i->maskColour(i,seg2), labels_map(seg2))
-    saveName = "$(homedir())/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/binarizeFuns/$(binarizeFunNames[n])FastScanningPrunedMask.png"
-    save(saveName,segmentedImage)
+    for (n,fun) in enumerate(binarizeFuns)
+
+        binarizedImage .= binarize(filteredImage,fun)
+        saveName = "$fileDirectory/$(f[1:end-4])/$(binarizeFunNames[n]).png"
+        save(saveName,binarizedImage)
+
+        seg = fast_scanning(binarizedImage, 0.1)
+        segmentedImage .= map(i->get_random_color(i), labels_map(seg))
+        saveName = "$fileDirectory/$(f[1:end-4])/$(binarizeFunNames[n])FastScanning.png"  # threshold = 0.1
+        save(saveName,segmentedImage)
+        seg2 = prune_segments(seg, i->(segment_pixel_count(seg,i)<30000), (i,j)->(-segment_pixel_count(seg,j)))
+        # segmentedImage .= map(i->get_random_color(i), labels_map(seg2))
+        # saveName = "$fileDirectory/$(f[1:end-4])/$(binarizeFunNames[n])FastScanningPruned.png"
+        # save(saveName,segmentedImage)
+        segmentedImage .= map(i->maskColour(i,seg2), labels_map(seg2))
+        saveName = "$fileDirectory/$(f[1:end-4])/$(binarizeFunNames[n])FastScanningPrunedMask.png"
+        save(saveName,segmentedImage)
+    end
 end
