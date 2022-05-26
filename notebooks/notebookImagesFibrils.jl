5de0b56c4de9e6fd1e368831a7670250334c6177
Christopher Revell <christopher.revell@manchester.ac.uk>
2022-05-26 10:55:04 +0100

    Fibril segmentation update

--- notebooks/notebookImagesFibrils.jl
+++ notebooks/notebookImagesFibrils.jl
@@ -41,5 +41,10 @@ binarizedImage = binarize(grayImage,Otsu())
 
 seg = fast_scanning(binarizedImage, 0.1)
 
-seg2 = prune_segments(seg, i->(segment_pixel_count(seg,i)<30000), (i,j)->(-segment_pixel_count(seg,j)))
-segmentedImage .= map(i->maskColour(i,seg2), labels_map(seg2))
+biggestSegment = sort(collect(seg.segment_pixel_count), by=x->x[2])[end][1]
+
+seg2 = prune_segments(seg, i->(segment_pixel_count(seg,i)>1200), (i,j)->(biggestSegment))
+seg3 = prune_segments(seg2, i->(segment_pixel_count(seg2,i)<500), (i,j)->(biggestSegment))
+segmentedImage = map(i->maskColour(i,seg), labels_map(seg))
+prunedImage1 = map(i->maskColour(i,seg2), labels_map(seg2))
+prunedImage2 = map(i->maskColour(i,seg3), labels_map(seg3))
