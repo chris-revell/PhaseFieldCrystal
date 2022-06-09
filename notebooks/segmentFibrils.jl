ef3511ce66471acbedc07356af6c5c4b66678ed4
Christopher Revell <christopher.revell@manchester.ac.uk>
2022-06-09 16:49:03 +0100

    Update segmentFibrils.jl

--- notebooks/segmentFibrils.jl
+++ notebooks/segmentFibrils.jl
@@ -15,6 +15,7 @@ using GR
 using GeometryBasics
 using LinearAlgebra
 using Statistics
+using ConcaveHull
 
 # Function to set random colour for each segment
 function get_random_color(seed)
@@ -45,10 +46,10 @@ binarizedImage = binarize(filteredImage,Otsu())
 
 # Erode and dilate
 doubleDilate = dilate(dilate(dilate(binarizedImage)))
-doubleErodeDilated = erode(erode(doubleDilate))
+# doubleErodeDilated = erode(erode(doubleDilate))
 
 # Initial segmentation of eroded and dilated image
-seg = fast_scanning(doubleErodeDilated, 0.01)
+seg = fast_scanning(doubleDilate, 0.01)
 
 # Prune segments below
 seg4 = prune_segments(seg, i->(segment_pixel_count(seg,i)<1000), (i,j)->(segment_pixel_count(seg,j)))
@@ -74,7 +75,7 @@ seg3 = prune_segments(seg2, i->(segment_pixel_count(seg2,i)<100), (i,j)->(-segme
 
 prunedImage2 = map(i->maskColour(i,seg3), labels_map(seg3))
 
-centroidLocations = Point2f[]
+centroidLocations = Point2[]
 for k in seg3.segment_labels
     pixels = zeros(2)
     count = 0
@@ -87,29 +88,42 @@ for k in seg3.segment_labels
         end
     end
     if count<1000
-        push!(centroidLocations,Point2f(pixels./count))
+        push!(centroidLocations,Point2(pixels./count))
     end
 end
-
 xs = [x[1] for x in centroidLocations]
 ys = [x[2] for x in centroidLocations]
+scalingFactor = maximum([xs ys])/(1-3eps(Float64))
+shiftedCentroidLocations = centroidLocations .+ Point2(0.0,size(image)[1]*1.0)
+shiftedCentroidLocations ./= scalingFactor
+
 n, tri = delaunay(xs,ys)
 nNeighbours = [length(findall(x->x==i,tri)) for i=1:length(centroidLocations)]
 
-# areas = abs.(area.([ centroidLocations[tri[i,:]] for i=1:n]))
+using ConcaveHull
+hull = concave_hull(shiftedCentroidLocations)
+hullInds = sort([findall(x->Point2(x...)==v,shiftedCentroidLocations)[1] for v in hull.vertices])
+
+internalCentroids = copy(shiftedCentroidLocations)
+deleteat!(internalCentroids,hullInds)
 
 pairs = Vector{Int64}[]
 lengths = Float64[]
 triEdges = [[1,2],[1,3],[2,3]]
 for t=1:size(tri)[1]
     for e in triEdges
-        if sort(tri[t,e]) ∉ pairs
+        if sort(tri[t,e]) ∉ pairs && tri[t,e][1] ∉ hullInds && tri[t,e][2] ∉ hullInds
             push!(pairs,sort(tri[t,e]))
-            push!(lengths,norm(centroidLocations[tri[t,e][1]].-centroidLocations[tri[t,e][2]]))
+            push!(lengths,norm(shiftedCentroidLocations[tri[t,e][1]].-shiftedCentroidLocations[tri[t,e][2]]))
         end
     end
 end
 lengths./=size(image)[2]
+deleteat!(lengths,hullInds)
+
+using VoronoiCells
+rect = Rectangle(Point2(0, 0), Point2(1, 1))
+tess = voronoicells(shiftedCentroidLocations, rect)
 
 set_theme!(figure_padding=1, backgroundcolor=(:white,1.0), font="Helvetica")
 fig = Figure(resolution=(1000,1000))
@@ -124,31 +138,37 @@ image!(axSegmented,rotr90(prunedImage2))
 
 axTriangle = CairoMakie.Axis(fig[2,1],aspect=DataAspect())
 hidedecorations!(axTriangle)
-scatter!(axTriangle,centroidLocations,color=:green,markersize=10)
-xlims!(axTriangle,0,size(image)[2])
-ylims!(axTriangle,-size(image)[1],0)
-areas = abs.(area.([ centroidLocations[tri[i,:]] for i=1:n]))
+scatter!(axTriangle,shiftedCentroidLocations,color=:green,markersize=10)
+xlims!(axTriangle,0,size(image)[2]/scalingFactor)
+ylims!(axTriangle,0,size(image)[1]/scalingFactor)
+areas = abs.(area.([ shiftedCentroidLocations[tri[i,:]] for i=1:n]))
 lims=(minimum(areas),maximum(areas))
 for i=1:n
-    poly!(axTriangle,centroidLocations[tri[i,:]],color=(:white,0.0),strokecolor=(:black,1.0),strokewidth=1.0)
+    poly!(axTriangle,shiftedCentroidLocations[tri[i,:]],color=(:white,0.0),strokecolor=(:black,1.0),strokewidth=1.0)
 end
 
 axNeighbour = CairoMakie.Axis(fig[2,2],aspect=DataAspect())
 hidedecorations!(axNeighbour)
+nNeighbours = [length(findall(x->x==i,tri)) for i=1:length(shiftedCentroidLocations)]
+xlims!(axNeighbour,0,size(image)[2]/scalingFactor)
+ylims!(axNeighbour,0,size(image)[1]/scalingFactor)
 # lims = (-maximum(abs.(nNeighbours)),maximum(abs.(nNeighbours)))
 coloursDict = Dict(2=>:black, 3=>:violet, 4=>:indigo, 5=>:blue, 6=>:white, 7=>:red, 8=>:orange)
 colours = [coloursDict[x] for x in nNeighbours]
-scatter!(axNeighbour,centroidLocations,color=colours,markersize=10)
-xlims!(axNeighbour,0,size(image)[2])
-ylims!(axNeighbour,-size(image)[1],0)
-
+# scatter!(axNeighbour,shiftedCentroidLocations,color=colours,markersize=10)
+poly!(axNeighbour,hull.vertices,color=(:black,0.5))
+for (i,c) in enumerate(tess.Cells)
+    if i ∉ hullInds
+        poly!(axNeighbour,c,color=colours[i])
+    end
+end
 
 axNeighbourHistogram = CairoMakie.Axis(fig[3,1], aspect=AxisAspect(1.4))
-counts = [count(x->x==y,nNeighbours) for y=minimum(nNeighbours):maximum(nNeighbours)]
+nNeighboursFiltered = [n for (i,n) in enumerate(nNeighbours) if i ∉ hullInds]
+counts = [count(x->x==y,nNeighboursFiltered) for y=minimum(nNeighbours):maximum(nNeighbours)]
 barplot!(axNeighbourHistogram,collect(minimum(nNeighbours):maximum(nNeighbours)),counts)
 axNeighbourHistogram.ylabel = "Count"
 axNeighbourHistogram.xlabel = "Number of neighbours"
-
 
 axLengths = CairoMakie.Axis(fig[3,2], aspect=AxisAspect(1.4))
 hist!(axLengths,lengths,bins=50)
@@ -162,13 +182,13 @@ Label(fig[2,2,Bottom()],L"d",textsize = 48)
 Label(fig[3,1,Bottom()],L"e",textsize = 48)
 Label(fig[3,2,Bottom()],L"f",textsize = 48)
 
-nVerts = length(centroidLocations)
+nVerts = length(shiftedCentroidLocations)
 nEdges = length(pairs)
 nFaces = length(tri)
 
 axText = CairoMakie.Axis(fig[4,:])
 text!(axText,
-    "Number of fibrils = $nVerts\nNumber of edges = $nEdges\nNumber of faces = $nCells\nMean edge length = $(mean(lengths))",
+    "Number of fibrils = $nVerts\nNumber of edges = $nEdges\nNumber of faces = $nFaces\nMean edge length = $(mean(lengths))",
     textsize=32,
     position = Point2(0.0,0.0),
     align = (:left, :center),
@@ -186,4 +206,4 @@ rowgap!(fig.layout,Relative(0.0))
 colgap!(fig.layout,Relative(0.01))
 resize_to_layout!(fig)
 display(fig)
-save("fibrilSegmentation.png",fig)
+save("$(datadir("plots"))/fibrilSegmentation.png",fig)
