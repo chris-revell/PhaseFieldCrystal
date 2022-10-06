8632767b1b692e6297f9cacd1a25f0636ac09240
chris-revell <christopher.revell@manchester.ac.uk>
2022-10-06 13:25:06 +0100

    Update to centroid neighbour plotting

--- notebooks/centroidsToMeasurements.jl
+++ notebooks/centroidsToMeasurements.jl
@@ -17,9 +17,20 @@ using DataFrames
 
 @from "$(projectdir("src","ColourFunctions.jl"))" using ColourFunctions
 
+function neighbourColours(x)
+    if x==6
+        return :white
+    elseif x==5 
+        return :red 
+    elseif x==7
+        return :blue
+    else 
+        return :black 
+    end
+end
 
 function centroidsToMeasurements(fileName)
-    mkpath(datadir("exp_pro","emCentroidMeasurements",splitpath(fileName)[end][1:end-4]))
+    mkpath(datadir("exp_pro","emCentroidNeighbours"))
     
     maskData = load(datadir("exp_pro","masks",splitpath(fileName)[end][1:end-4],"$(splitpath(fileName)[end][1:end-4]).jld2"))
     @unpack newIndexMap, lX, h = maskData
@@ -52,7 +63,7 @@ function centroidsToMeasurements(fileName)
     tess = voronoicells(shiftedCentroidLocations, Rectangle(Point2(0, 0), Point2(1, 1)))
 
 
-    fig = Figure()
+    fig = Figure(resolution=(1000,500))
     ax1 = CairoMakie.Axis(fig[1,1],aspect=DataAspect())
     hidedecorations!(ax1)
     hidespines!(ax1)
@@ -66,54 +77,47 @@ function centroidsToMeasurements(fileName)
     # Plot voronoi cells around each fibril coloured by neighbour count
 
     
-    # maskImage = copy(imageIn)
-    # for i=1:size(imageIn)[1]
-    #     for j=1:size(imageIn)[2]
-    #         if newIndexMap[i,j] == 0
-    #             maskImage[i,j] == (:black,0.0)
-    #         else
-    #             maskImage[i,j] == (:black,1.0)
-    #         end
-    #     end
-    # end
+    maskImage = fill(RGBA(1,1,1,1),size(imageIn))
+    for i=1:size(imageIn)[1]
+        for j=1:size(imageIn)[2]
+            if newIndexMap[i,j] < 0.5
+                maskImage[i,j] = RGBA(0.0,0.0,0.0,1.0)
+            else
+                maskImage[i,j] = RGBA(0.0,0.0,0.0,0.0)
+            end
+        end
+    end    
 
-    # Map neighbour counts to colours 
-    cmap = cgrad(:bwr, 5, categorical=true, rev=true)
+ 
+
+
     for (i,c) in enumerate(tess.Cells)
-        # if i ∉ hullInds
-            poly!(ax2, c, color=[nNeighbours[i]], colormap=cmap, colorrange=(4,8),highclip=:black,lowclip=:black)
-        # end
+        if i ∉ hullInds
+            poly!(ax2, c.*scalingFactor, color=neighbourColours(nNeighbours[i]),strokecolor=(:black,1.0),strokewidth=1.0)
+        else
+            # poly!(ax2, c.*scalingFactor, color=:white,strokecolor=(:black,1.0),strokewidth=1.0)
+        end
     end
-
-    poly!(ax2,hull.vertices,color=(:grey,1.0))
-
-    # image!(ax2,maskImage)
+    image!(ax2,rotr90(maskImage))
+    # poly!(ax2,hull.vertices,color=(:grey,1.0))
+    scatter!(ax2,centroidLocations,color=(:orange,1.0),markersize=4)
     hidedecorations!(ax2)
     hidespines!(ax2)
-    xlims!(ax2,0,size(imageIn)[2]/scalingFactor)
-    ylims!(ax2,0,size(imageIn)[1]/scalingFactor)
-    
+    resize_to_layout!(fig)
     display(fig)
-    # if saveFlag == 1
-        # save(datadir("exp_pro","emCentroidMeasurements",splitpath(fileName)[end][1:end-4],"$(splitpath(fileName)[end])"),fig)
-        # save(datadir("exp_pro","emCentroidMeasurements",splitpath(fileName)[end][1:end-4],"$(splitpath(fileName)[end][1:end-4]).jld2"),@strdict fileName fibrilMinSize distanceGaussian dilateCount erodeCount centroidLocations fig)
-    # end
-    # display(fig)
-    # display(maximum(nNeighbours))
-    # display(minimum(nNeighbours))
-    
-    return fig, centroidLocations, newIndexMap
+
+    save(datadir("exp_pro","emCentroidNeighbours","$(splitpath(fileName)[end])"),fig)
 end
 
 # fileName = "/Users/christopher/Postdoc/Code/PhaseFieldCrystal/data/exp_pro/cropped/mp13ko-3wiew_4800X_hui_0002_2.png"
 runs = [f for f in readdir(datadir("exp_pro","masks","ok")) if f[end-3:end]==".png"]
-lengthMeasurements = DataFrame(CSV.File(datadir("exp_pro","lengthMeasurements","lengthMeasurements.csv")))
-distanceGaussian = 2.0
-fibrilMinSize = 10
-dilateCount = 2
-erodeCount = 2
-fibrilMaxSize = 5000
-saveFlag=1
+# lengthMeasurements = DataFrame(CSV.File(datadir("exp_pro","lengthMeasurements","lengthMeasurements.csv")))
+# distanceGaussian = 2.0
+# fibrilMinSize = 10
+# dilateCount = 2
+# erodeCount = 2
+# fibrilMaxSize = 5000
+# saveFlag=1
 for r in runs
     centroidsToMeasurements(datadir("exp_pro","cropped",r))
 end
