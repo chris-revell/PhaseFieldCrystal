309c6e22194f60ea8ab85f94eb2d724b9ca32c28
Christopher Revell <christopher.revell@manchester.ac.uk>
2022-10-27 11:46:01 +0100

    Update allMaskSimulationsMeasurements.jl

--- notebooks/allMaskSimulationsMeasurements.jl
+++ notebooks/allMaskSimulationsMeasurements.jl
@@ -44,6 +44,8 @@ for r in runs
 
     maskIn = load(datadir("exp_pro","masksCompressed",mask,"$mask.png"))
     nX = size(maskIn)[2]
+    imageIn = load(datadir("exp_pro","cropped","$mask.png"))
+
     subsetCroppedLX = filter(:file => m -> occursin.(mask,m), croppedLX)
     lengthPerPixelMask = subsetCroppedLX[1,:lX]/nX
 
@@ -53,20 +55,24 @@ for r in runs
     fig = Figure(resolution=(2000,1000),fontsize=32)
     ax1 = CairoMakie.Axis(fig[1,2])
     ax2 = CairoMakie.Axis(fig[1,3])
-    ax1.title = "Simulations spacing"
+    ax1.title = "Spacing distribution"
     ax1.xlabel = "Edge length/nm"
     ax1.ylabel = "Density"
-    ax2.title = "Simulation neighbour distribution"
+    ax2.title = "Neighbour distribution"
     ax2.xlabel = "Neighbour count"
     ax2.ylabel = "Density"
-    ax3 = CairoMakie.Axis(fig[2,2])
-    ax4 = CairoMakie.Axis(fig[2,3])
-    ax3.title = "Em spacing"
-    ax3.xlabel = "Edge length/nm"
-    ax3.ylabel = "Density"
-    ax4.title = "EM neighbour distribution"
-    ax4.xlabel = "Neighbour count"
-    ax4.ylabel = "Density"
+    ax3 = CairoMakie.Axis(fig[1,1],aspect=DataAspect())
+    hidedecorations!(ax3)
+    hidespines!(ax3)
+    image!(rotr90(imageIn))
+    # ax3 = CairoMakie.Axis(fig[2,2])
+    # ax4 = CairoMakie.Axis(fig[2,3])
+    # ax3.title = "Em spacing"
+    # ax3.xlabel = "Edge length/nm"
+    # ax3.ylabel = "Density"
+    # ax4.title = "EM neighbour distribution"
+    # ax4.xlabel = "Neighbour count"
+    # ax4.ylabel = "Density"
 
     # ylims!(ax1,(0,1))
     # ylims!(ax2,(0,1))
@@ -127,14 +133,14 @@ for r in runs
     for i=1:length(hNorm.weights)
         push!(points,Point2(mean(edgeVec[i:i+1]),hNorm.weights[i]))
     end
-    lines!(ax3,points)
+    lines!(ax1,points,linestyle="--",color=:black,label="EM data")
     points = Point2[]
     hNorm = normalize(fit(Histogram, nNeighbours[filter(x->x ∉ hullInds, eachindex(nNeighbours))], 0.5:11.5), mode=:pdf)
     edgeVec = collect(hNorm.edges[1])    
     for i=1:length(hNorm.weights)
         push!(points,Point2(mean(edgeVec[i:i+1]),hNorm.weights[i]))
     end
-    lines!(ax4,points)
+    lines!(ax2,points,linestyle="--",color=:black,label="EM data")
         
     # Loop to process data from each run 
     for i=1:nrow(results)
@@ -237,6 +243,9 @@ for r in runs
 
         display(mean(lengths))
     end 
+
+    axislegend(ax1, position = :rt)
+    axislegend(ax2, position = :rt)
 
     resize_to_layout!(fig)
 
