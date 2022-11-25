3dddf577dd65e447323722cb82f4bcc1b423e0ee
Christopher Revell <christopher.revell@manchester.ac.uk>
2022-11-25 13:03:33 +0000

    Update CreateDivAlphaGrad.jl

--- src/CreateDivAlphaGrad.jl
+++ src/CreateDivAlphaGrad.jl
@@ -18,7 +18,7 @@ using FromFile: @from
 
 function createDivAlphaGrad(nX, nY, h, α)
 
-    incidence = spzeros(2*nX*nY,nX*nY)
+    incidence = spzeros(2*nX*nY,nX*nY) # Incidence matric mapping edges between grid points to grid points themselves 
     for x=1:nX
         for y=1:nY
             # Index of grid point (x,y) when 2D array is flattened to a 1D vector
@@ -27,29 +27,31 @@ function createDivAlphaGrad(nX, nY, h, α)
             # Loop over all edges neighbouring vertex (x,y)
             # Edges stored as vector of size 2*nX*nY.
             # First half of vector stores x directed edges; second half stores y directed edges.
-            # Incidence matrix maps each vertex to 2 x directed edges with + and - directions, and 2 y directed edges with + and - directions
+            # Incidence matrix maps each vertex to 2 x-directed edges with + and - directions, and 2 y-directed edges with + and - directions
 
-            # x dimension edge neighbours
+            #  Find indices of x-direction edges surrounding grid point (x,y) ie (x-1,y) and (x,y)
+            # Trailing edge:
             xNew = arrayLoop(x-1,nX)
             yNew = y
-            indexEdge = (xNew-1)*nY + yNew
-            incidence[indexEdge,indexVertex] = -1
-
+            indexEdge = (xNew-1)*nY + yNew        #  Convert cartesian indices of edges to within flattened vector
+            incidence[indexEdge,indexVertex] = -1 # Set incidence matrix component to -1 for trailing edge
+            # Leading edge:
             xNew = x
             yNew = y
-            indexEdge = (xNew-1)*nY + yNew
-            incidence[indexEdge,indexVertex] = 1
+            indexEdge = (xNew-1)*nY + yNew        #  Convert cartesian indices of edges to within flattened vector
+            incidence[indexEdge,indexVertex] = 1  # Set incidence matrix component to 1 for leading edge
 
-            # y dimension edge neighbours
+            #  Find indices of y-direction edges surrounding grid point (x,y) ie (x,y-1) and (x,y)
+            #  Trailing edge
             xNew = x
             yNew = arrayLoop(y-1,nY)
-            indexEdge = (xNew-1)*nY + yNew + nX*nY
-            incidence[indexEdge,indexVertex] = -1
-
+            indexEdge = (xNew-1)*nY + yNew + nX*nY #  Convert cartesian indices of edges to within flattened vector, NB adding NxNy to put y-directed components in 2nd half of vector 
+            incidence[indexEdge,indexVertex] = -1  # Set incidence matrix component to -1 for trailing edge
+            #  Leading edge
             xNew = x
             yNew = y
-            indexEdge = (xNew-1)*nY + yNew + nX*nY
-            incidence[indexEdge,indexVertex] = 1
+            indexEdge = (xNew-1)*nY + yNew + nX*nY #  Convert cartesian indices of edges to within flattened vector, NB adding NxNy to put y-directed components in 2nd half of vector 
+            incidence[indexEdge,indexVertex] = 1   # Set incidence matrix component to 1 for trailing edge
 
         end
     end
