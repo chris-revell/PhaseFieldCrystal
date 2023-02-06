0b2f364e3f76e7b8cfbe082c789972e0c81de1ae
Christopher Revell <christopher.revell@manchester.ac.uk>
2023-02-06 12:08:49 +0000

    Update msDataPlot.jl

--- notebooks/forPaper/msDataPlot.jl
+++ notebooks/forPaper/msDataPlot.jl
@@ -10,25 +10,44 @@ end
 
 xf = XLSX.readxlsx("/Users/christopher/Dropbox (The University of Manchester)/RevellHerrera1/MS_Data/extractedMSvalues.xlsx")
 
-fig = CairoMakie.Figure(resolution=(800,500))
+t = [12.5,13.0, 13.5, 14.0, 14.5]
+
+colors = [:black, :red, :green, :blue, :yellow]
+
+fig = CairoMakie.Figure(resolution=(500,500))
 ax = Axis(fig[1,1])
-ax.xlabel = "Day /E"
-ax.ylabel = "Percentage change"
-
-t = [13.0, 13.5, 14.0, 14.5]#(xf[1]["A3:A6"])[:,1]
-
-for i=1:7
-    means = Float64[]
-    sems = Float64[]
+ax.xlabel = "Day"
+ax.ylabel = "log₂ of fold change in expression vs E12.5"
+for i=1:4
+    means = [0.0]
+    sems = [0.0]
     datalabel = xf[1][1,(i-1)*3+2]
     for j=3:6
         push!(means,xf[1][j,(i-1)*3+2])
         push!(sems,xf[1][j,(i-1)*3+3])
     end    
-    lines!(ax,t,means,color=getRandomColor(i))
-    errorbars!(ax,t,means,sems,label=datalabel,color=getRandomColor(i))
+    lines!(ax,t,means,color=colors[i])
+    errorbars!(ax,t,means,sems,label=datalabel,color=colors[i])
 end
+legend = Legend(fig[1,1],ax,tellheight = false, tellwidth = false, margin = (10, 10, 10, 10), halign = :right, valign = :top)#, orientation = :horizontal
+# display(fig)
+save("/Users/christopher/Dropbox (The University of Manchester)/RevellHerrera1/MS_Data/extractedMSvalues1.png",fig)
 
-legend = Legend(fig[1,2],ax)
-
-display(fig)
+fig = CairoMakie.Figure(resolution=(500,500))
+ax = Axis(fig[1,1])
+ax.xlabel = "Day"
+ax.ylabel = "log₂ of fold change in expression vs E12.5"
+for i=5:7
+    means = [0.0]
+    sems = [0.0]
+    datalabel = xf[1][1,(i-1)*3+2]
+    for j=3:6
+        push!(means,xf[1][j,(i-1)*3+2])
+        push!(sems,xf[1][j,(i-1)*3+3])
+    end    
+    lines!(ax,t,means,color=colors[i-4])
+    errorbars!(ax,t,means,sems,label=datalabel,color=colors[i-4])
+end
+legend = Legend(fig[1,1],ax,tellheight = false, tellwidth = false, margin = (10, 10, 10, 10), halign = :right, valign = :top)#, orientation = :horizontal
+# display(fig)
+save("/Users/christopher/Dropbox (The University of Manchester)/RevellHerrera1/MS_Data/extractedMSvalues2.png",fig)
