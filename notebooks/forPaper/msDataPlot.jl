28cb965075ed4113946d444476d72677023d4b00
Christopher Revell <christopher.revell@manchester.ac.uk>
2023-02-07 09:56:59 +0000

    Update msDataPlot.jl

--- notebooks/forPaper/msDataPlot.jl
+++ notebooks/forPaper/msDataPlot.jl
@@ -14,7 +14,7 @@ t = [12.5,13.0, 13.5, 14.0, 14.5]
 
 colors = [:black, :red, :green, :blue, :yellow]
 
-fig = CairoMakie.Figure(resolution=(500,500))
+fig = CairoMakie.Figure(resolution=(750,750), fontsize=32)
 ax = Axis(fig[1,1])
 ax.xlabel = "Day"
 ax.ylabel = "log₂ of fold change in expression vs E12.5"
@@ -30,10 +30,10 @@ for i=1:4
     errorbars!(ax,t,means,sems,label=datalabel,color=colors[i])
 end
 legend = Legend(fig[1,1],ax,tellheight = false, tellwidth = false, margin = (10, 10, 10, 10), halign = :right, valign = :top)#, orientation = :horizontal
-# display(fig)
+
 save("/Users/christopher/Dropbox (The University of Manchester)/RevellHerrera1/MS_Data/extractedMSvalues1.png",fig)
 
-fig = CairoMakie.Figure(resolution=(500,500))
+fig = CairoMakie.Figure(resolution=(750,750), fontsize=32)
 ax = Axis(fig[1,1])
 ax.xlabel = "Day"
 ax.ylabel = "log₂ of fold change in expression vs E12.5"
