a9042265a333327f0ef49b6fca6efc117c7a67e8
Christopher Revell <christopher.revell@manchester.ac.uk>
2023-08-14 16:49:45 +0100

    fibrils -> A B incidence matrices

--- notebooks/fibrilsABmatrices.jl
+++ notebooks/fibrilsABmatrices.jl
@@ -8,7 +8,7 @@ using FromFile: @from
 using CairoMakie
 using Makie.GeometryBasics
 using DelaunayTriangulation
-
+using ConcaveHull
 
 # Import local files
 @from "$(srcdir("CreateLaplacian.jl"))" using CreateLaplacian
@@ -32,92 +32,53 @@ data = load(datadir("exp_pro","emCentroidsInteractive",runs[1]))
 
 @unpack centroidLocations = data
 
-# boundingRectangle = [
-#     Point2(0.0,0.0), 
-#     Point2(700.0, 0.0), 
-#     Point2(700.0,700.0), 
-#     Point2(0.0,700.0)
-# ]
-# p = Polygon(
-#     boundingRectangle,
-#     [hullPoints]
-# )
-# poly!(ax,p,color=:blue)
-
-# Find Delaunay triangulation and Voronoi tessellation
-pointsArray = zeros(Float64,(2,length(centroidLocations)))
+ptsArray = zeros(Float64,(2,length(centroidLocations)))
 for (i,point) in enumerate(centroidLocations)
-    pointsArray[1,i] = point[1]
-    pointsArray[2,i] = point[2]
-end
-triangulation = triangulate(pointsArray)
-tessellation = voronoi(triangulation, true)
-
-# Convert vertices of Voronoi tessellation to Point2 type
-vertexPoints = Point2.(tessellation.polygon_points)
-
-
-# Find all edges in Delaunay triangulation
-# NB get_edges(triangulation) returns the number of edges including ghost get_edges
-# Exclude ghost edges by removing pairs with negative vertex indices
-# Use collect to convert set to vector
-pairsInTriangulation = collect(filter(x->min(x...)>0,get_edges(triangulation)))
-# for p in pairsInTriangulation
-#     lines!(ax,centroidLocations[[p...]],color=:black)
-# end
-
-edgesInTessellation = Tuple{Int64, Int64}[]
-for p in pairsInTriangulation
-    # Find tessellation vertices shared by points at both ends of pair p in triangulation
-    # Note that the list of pairs does not include ghost edges in the triangulation so there should be no ghost points here...
-    tessellationVertices = tessellation.polygons[p[1]]∩tessellation.polygons[p[2]]
-    push!(edgesInTessellation,Tuple(tessellationVertices))
-end
-# Add convex hull as edges 
-# Indices in convex hull object are correctly ordered anti-clockwise already, 
-# and start and end with the same index
-# Note, need to relabel indices for use in A, B matrices, 
-# so their indices are all > max index of vertices in tessellation 
-for i=2:length(triangulation.convex_hull.indices)
-    edgePair = (num_polygon_vertices(tessellation)+i,num_polygon_vertices(tessellation)+i-1)
-    push!(edgesInTessellation,edgePair)
+    ptsArray[1,i] = point[1]
+    ptsArray[2,i] = point[2]
 end
 
-# R matrix of vertices for A, B matrices includes vertices in tessellation (excluding ghost vertices) 
-# and vertices in convex hull of triangulation
-R = tessellation.polygon_points
-# for i=1:length(triangulation.convex_hull.indices)-1
-#     push!(R,Tuple(triangulation.convex_hull.points[:,i]))
-# end
+triangulation_unconstrained = triangulate(ptsArray)
+# tessellation_unconstrained = voronoi(triangulation_unconstrained)
+tessellation_constrained = voronoi(triangulation_unconstrained,true)
+
+R = [get_polygon_point(tessellation_constrained,i) for i=1:num_polygon_vertices(tessellation_constrained)]
+
+
+orderedPairs = unique([(min(p...),max(p...)) for p in keys(tessellation_constrained.adjacent.adjacent)])
+
+nVerts = length(R)
+nEdges = length(orderedPairs)
+nCells = length(centroidLocations)
 
 # Construct A matrix mapping tessellation edges to tessellation vertices 
-A = spzeros(length(edgesInTessellation),num_polygon_vertices(tessellation))
-for (edgeIndex,vertices) in enumerate(edgesInTessellation)
+A = spzeros(nEdges,nVerts)
+for (edgeIndex,vertices) in enumerate(orderedPairs)
     A[edgeIndex,vertices[1]] = 1
     A[edgeIndex,vertices[2]] = -1
 end
 
-# B = spzeros(length(centroidLocations),length(pairs))
-# for c=1:length(centroidLocations)
-#     verticesOfCell = tessellation.polygons[c]
-#     for e=2:length(verticesOfCell)
-#         edge = findall(x->x==(e,e-1)||x==(e-1,e), pairs)[1]
-#         if A[edge,verticesOfCell[e]] == 1
-#             B[c,edge] = 1
-#         else
-#             B[c,edge] = -1
-#         end
-#     end
-# end
 
+# NB get_polygon(tessellation_constrained,x) or tessellation_constrained.polygons[x] return indices of vertices around cell x ordered anti-clockwise, with first and last element the same
 
-fig = Figure(resolution=(750,750)); ax=Axis(fig[1,1],aspect=DataAspect())
-xlims!(ax,0.9*minimum(first.(centroidLocations)),1.05*maximum(first.(centroidLocations)))
-ylims!(ax,0.9*minimum(last.(centroidLocations)),1.05*maximum(last.(centroidLocations)))
-hidedecorations!(ax)
-hidespines!(ax)
-# scatter!(ax,first.(R),last.(R))
-# scatter!(ax,centroidLocations, color=:red)
-voronoiplot!(ax, tessellation, strokecolor=:red, strokewidth=0.2, show_generators=false)
-triplot!(ax, triangulation, strokewidth=0.2, strokecolor=(:black, 0.4), show_convex_hull=true)
+# Construct B matrix mapping voronoi cell around each fibril to surrounding edges between vertices in tessellation
+B = spzeros(nCells,nEdges)
+for c=1:nCells
+    for i=2:length(tessellation_constrained.polygons[c]) # iterating around polygon anti-clockwise 
+        vertexLeading = tessellation_constrained.polygons[c][i]  # Leading with respect to anticlockwise direction around cell
+        vertexTrailing = tessellation_constrained.polygons[c][i-1]                
+        # Find index of edge connecting these vertices 
+        edge = (findall(x->x!=0,A[:,vertexLeading])∩findall(x->x!=0,A[:,vertexTrailing]))[1]        
+        if A[edge,vertexLeading] > 0
+            B[c,edge] = 1
+        else
+            B[c,edge] = -1
+        end
+    end
+end
+
+fig, ax, sc = voronoiplot(tessellation_constrained, strokecolor=:red, markersize=9)
+# fig = Figure(resolution=(750,750)); ax=Axis(fig[1,1],aspect=DataAspect())
+# triplot!(ax, triangulation_unconstrained, strokewidth=1, strokecolor=(:black, 0.4))
+# annotations!(ax,string.(collect(1:length(centroidLocations))),centroidLocations,color=:red)
 display(fig)
